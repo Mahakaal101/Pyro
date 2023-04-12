81420b5d4190ad1d161a907b8ee686d657c58da9
Mahakaal101 <124979598+Mahakaal101@users.noreply.github.com>
2023-04-12 15:49:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "15823382")
+API_ID = os.environ.get("API_ID", "26305247")
 
-API_HASH = os.environ.get("API_HASH", "016d5e115a06ddfb6121823d72ae4d8c")
+API_HASH = os.environ.get("API_HASH", "20ca7e6687c281e11782856c7efd0ff7")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5945280153:AAEZFpMp-Plgwgoce0uVkT7RKk6rLTngCSA") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6132391938:AAEPDCNePTVGl-3UGz3lb4vsVZ6fRDiH9Bs") 
 
 FORCE_SUB = os.environ.get("FORCE_SUB", "Film_Update_Official") 
 
@@ -14,12 +14,12 @@ DB_NAME = os.environ.get("DB_NAME","Cluster0")
 
 DB_URL = os.environ.get("DB_URL","mongodb+srv://Dipanshu_021:ad8920@cluster0.f7migc1.mongodb.net/?retryWrites=true&w=majority")
  
-FLOOD = int(os.environ.get("FLOOD", "10"))
+FLOOD = int(os.environ.get("FLOOD", "0"))
 
 START_PIC = os.environ.get("START_PIC", "https://telegra.ph/file/ae8701b26c6cf0db98c0b.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5791145987').split()]
 
-PORT = os.environ.get('PORT', '8000')
+PORT = os.environ.get('PORT', '80')
 
 WEBHOOK = bool(os.environ.get("WEBHOOK", True))
